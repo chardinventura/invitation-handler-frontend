5849a8cbe71620b628b550f8d605b98df623f6c3
chardindelacruz <chardin.delacruz-ventura@viseo.com>
2022-12-19 11:12:31 -0400

    Vista para la confirmacion de asistencia

---
+++ src/modules/app/attendanceConfirmation/attendanceConfirmation.js
@@ -0,0 +1,17 @@
+import { api, LightningElement } from "lwc";
+import { View } from 'util/constants';
+import { navigate } from 'util/functions';
+
+export default class AttendanceConfirmation extends LightningElement {
+
+	@api
+	selectedInvitation;
+
+	handleConfirmation() {
+		navigate.bind(this)(View.ATTENDANCE, false);
+	}
+
+	handleAbort() {
+		navigate.bind(this)(View.INVITATIONS);
+	}
+}
